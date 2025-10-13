fed7be47dac65c69e1b1569dc756e5a5eb7b6339
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-10-13 11:30:48 +0700

    fix: remove file and unused code

--- src/stores/main.js
+++ src/stores/main.js
@@ -30,10 +30,6 @@ export const useMainStore = defineStore('main', () => {
   const listIspuPM25 = ref([]);
   const listDailyIspu = ref([])
   const listDaily30Minute = ref([]);
-  const presenceToday = ref([])
-  const presenceMonthly = ref([])
-  const presenceRangeByDate = ref([])
-  const presenceByPerson = ref([])
 
   function setUser(payload) {
     if (payload.name) {
@@ -195,108 +191,6 @@ export const useMainStore = defineStore('main', () => {
       })
   }
 
-  function fetchPresenceToday() {
-    axios.get('https://api-absensi.getsensync.com/api/absensi/rekap').then((result) => {
-      const data = result?.data || []
-
-      const processedData = data.map((item) => {
-        const result_diff = DateFormatter.getDifferenceHour(item.waktu_masuk, item.terakhir_terlihat)
-
-        return {
-          ...item,
-          terakhir_terlihat: DateFormatter.convertToTime(item.terakhir_terlihat),
-          waktu_masuk: DateFormatter.convertToTime(item.waktu_masuk),
-          waktu_keluar: DateFormatter.convertToTime(item.waktu_keluar),
-          tanggal: DateFormatter.convertToDate(item.terakhir_terlihat),
-          jumlah_jam: result_diff
-        }
-      })
-
-      presenceToday.value = processedData
-    })
-      .catch((error) => {
-        alert(error.message)
-      })
-  }
-
-  function fetchPresenceMonthly(date) {
-    axios.get(`https://api-absensi.getsensync.com/api/absensi/rekap/bulanan?bulan=${date}`).then((result) => {
-      presenceMonthly.value = result?.data.sort((a, b) => b.jumlah_hadir - a.jumlah_hadir)
-    })
-      .catch((error) => {
-        alert(error.message)
-      })
-  }
-
-  function fetchRangePresence(startDate, endDate) {
-    var url = '';
-
-    if (startDate && endDate) {
-      url = `https://api-absensi.getsensync.com/api/absensi/rekap/rekap-range?start=${startDate}&end=${endDate}`;
-    } else {
-      url = `https://api-absensi.getsensync.com/api/absensi/rekap/${startDate}`
-    }
-    axios.get(url).then((result) => {
-      const data = result?.data || []
-
-      const processedData = data.map((item) => {
-        const result_diff = DateFormatter.getDifferenceHour(item.waktu_masuk, item.terakhir_terlihat)
-
-        return {
-          ...item,
-          terakhir_terlihat: DateFormatter.convertToTime(item.terakhir_terlihat),
-          waktu_masuk: DateFormatter.convertToTime(item.waktu_masuk),
-          waktu_keluar: DateFormatter.convertToTime(item.waktu_keluar),
-          tanggal: DateFormatter.convertToDate(item.terakhir_terlihat),
-          jumlah_jam: result_diff
-        }
-      })
-
-      presenceRangeByDate.value = processedData
-    })
-      .catch((error) => {
-        alert(error.message)
-      })
-  }
-
-  function fetchPresencePerson(mac_address, filterMonth) {
-    axios.get(`https://api-absensi.getsensync.com/api/absensi/rekap/detail/${mac_address}`).then((result) => {
-      const allData = result?.data || []
-
-      const allProcessedData = allData.map((item) => {
-        const result_diff = DateFormatter.getDifferenceHour(item.waktu_masuk, item.terakhir_terlihat)
-
-        return {
-          ...item,
-          jumlah_jam: result_diff
-        }
-      })
-
-      const filtered = allProcessedData.filter((item) => {
-        const seenMonth = new Date(item.terakhir_terlihat).toISOString().slice(0, 7) // "YYYY-MM"
-        return seenMonth === filterMonth
-      })
-
-      const resultData = filtered.map((item) => {
-        return {
-          ...item,
-          terakhir_terlihat: DateFormatter.convertToTime(item.terakhir_terlihat),
-          waktu_masuk: DateFormatter.convertToTime(item.waktu_masuk),
-          waktu_keluar: DateFormatter.convertToTime(item.waktu_keluar),
-          tanggal: DateFormatter.convertToDate(item.terakhir_terlihat),
-        }
-      })
-
-
-
-      presenceByPerson.value = resultData
-
-    })
-      .catch((error) => {
-        alert(error.message)
-      })
-  }
-
   return {
     userName,
     userEmail,
@@ -311,19 +205,11 @@ export const useMainStore = defineStore('main', () => {
     listIspuPM10,
     listIspuPM25,
     listDaily30Minute,
-    presenceToday,
-    presenceMonthly,
-    presenceRangeByDate,
-    presenceByPerson,
     setUser,
     fetchIspuLatest,
     fetchIspuDaily,
     fetch30Minute,
     fetchSampleClients,
     fetchSampleHistory,
-    fetchPresenceToday,
-    fetchPresenceMonthly,
-    fetchRangePresence,
-    fetchPresencePerson,
   }
 })
