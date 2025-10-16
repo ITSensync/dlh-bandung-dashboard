4ae28f164d243574bdcb6dfcd4766d837054d6ae
Alief Muhammad Abdillah <aliefmabdillah09@gmail.com>
2025-10-16 11:16:07 +0700

    fix: remove coordinat in navbar

--- src/components/NavbarGuest.vue
+++ src/components/NavbarGuest.vue
@@ -18,22 +18,6 @@ onMounted(() => {
         <a class="text-xs sm:text-xl font-poppins font-extrabold"
           >Pemantauan Kualitas Udara Kota Bandung</a
         >
-        <div class="flex flex-row gap-2 font-poppins font-semibold">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            class="size-4 sm:size-6 fill-dlh_green"
-          >
-            <path
-              fill-rule="evenodd"
-              d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          <p>6.78 LU</p>
-          <p>145.41 LS</p>
-        </div>
       </div>
     </div>
     <div class="hidden sm:flex flex-col items-center justify-center bg-dlh_green font-poppins px-2">
